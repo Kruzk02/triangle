b1ec6e109e785651e2523371a0a65210b3adb652
Thien phuc <thienphuc484@gmail.com>
2025-12-13 14:32:49 +0700

    Add stb_image

--- asset/shader/shader.fs
+++ asset/shader/shader.fs
@@ -2,7 +2,10 @@
 
 out vec4 fragColor;
 
+in vec2 vUV;
+
 uniform float uTime;
+uniform sampler2D uTexture;
 
 float rand(vec2 co)
 {
@@ -15,7 +18,6 @@ void main()
     float seed = floor(uTime / interval);
     float t = mod(uTime, interval) / interval;
 
-    // Top color
     vec3 topA = vec3(
         rand(vec2(seed, 1.0)),
         rand(vec2(seed, 2.0)),
@@ -23,27 +25,32 @@ void main()
     );
 
     vec3 topB = vec3(
-        rand(vec2(seed+1, 1.0)),
-        rand(vec2(seed+1, 2.0)),
-        rand(vec2(seed+1, 3.0))
+        rand(vec2(seed + 1.0, 1.0)),
+        rand(vec2(seed + 1.0, 2.0)),
+        rand(vec2(seed + 1.0, 3.0))
     );
 
-    // Bottom color
     vec3 bottomA = vec3(
-        rand(vec2(seed+2, 1.0)),
-        rand(vec2(seed+2, 2.0)),
-        rand(vec2(seed+2, 3.0))
+        rand(vec2(seed + 2.0, 1.0)),
+        rand(vec2(seed + 2.0, 2.0)),
+        rand(vec2(seed + 2.0, 3.0))
     );
 
     vec3 bottomB = vec3(
-        rand(vec2(seed+3, 1.0)),
-        rand(vec2(seed+3, 2.0)),
-        rand(vec2(seed+3, 3.0))
+        rand(vec2(seed + 3.0, 1.0)),
+        rand(vec2(seed + 3.0, 2.0)),
+        rand(vec2(seed + 3.0, 3.0))
     );
 
     vec3 topColor = mix(topA, topB, t);
     vec3 bottomColor = mix(bottomA, bottomB, t);
 
-    vec3 finalColor = mix(bottomColor, topColor, gl_FragCoord.y / 720);
-    fragColor = vec4(finalColor, 1.0);
+    float gradient = gl_FragCoord.y / 720.0;
+    vec3 proceduralColor = mix(bottomColor, topColor, gradient);
+
+    vec3 textureColor = texture(uTexture, vUV).rgb;
+
+    vec3 finalColor = proceduralColor * textureColor;
+
+    fragColor = texture(uTexture, vUV);
 }
